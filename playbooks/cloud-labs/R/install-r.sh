2de627c728f2cca22c664d4543e1771b66bdeb4f
Jillian Rowe <jillian.e.rowe@gmail.com>
2022-11-02 11:34:39 +0300

    commit

---
+++ playbooks/cloud-labs/R/install-r.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#SBATCH --partition dev
+#SBATCH --constraint m5a4xlarge
+#SBATCH --time 1-00:00:00
+#SBATCH --exclusive
+
+export SPACK_ROOT=/apps/spack/0.19.0
+source /apps/spack/0.19.0/share/spack/setup-env.sh
+
+# First try to install with the binary cache
+spack install \
+  -j 4 \
+  --no-check-signature \
+  --no-checksum \
+  --use-cache \
+  --reuse    \
+  r-tidyverse@1.3.2 r-seurat
+
+spack install \
+  -j 4 \
+  --no-check-signature \
+  --no-checksum \
+  --no-cache \
+  --reuse    \
+  r-tidyverse@1.3.2 r-seurat
