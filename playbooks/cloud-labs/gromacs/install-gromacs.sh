8cc84d898a878bb9ad8d958293862d98bc830774
Jillian Rowe <jillian.e.rowe@gmail.com>
2023-03-08 20:10:24 +0300

    commit

--- playbooks/cloud-labs/gromacs/install-gromacs.sh
+++ playbooks/cloud-labs/gromacs/install-gromacs.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
+#SBATCH -N 4
+#SBATCH -n 4
 #SBATCH --partition dev
 #SBATCH --constraint m5a4xlarge
 #SBATCH --time 1-00:00:00
 #SBATCH --exclusive
 
 # First try to install with the binary cache
-spack install \
+srun -v -v -N 4 -n 4 spack install \
+  -v -y \
+  --deprecated \
+  --show-log-on-error \
   --no-check-signature \
   --no-checksum \
-  gromacs@2021 \
   gromacs@2022 \
   gromacs@2022 +cuda +mpi \
-  gromacs@2022 +cuda ~mpi \
-  gromacs@2021 +cuda ~mpi \
-  gromacs@2021 +cuda +mpi \
-  gromacs@2022 +openmp +blas +lapack \
-  gromacs@2021 +openmp +blas +lapack
+  gromacs@2022 +cuda ~mpi
 
--- playbooks/cloud-labs/relion/install-relion.sh
+++ playbooks/cloud-labs/relion/install-relion.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+#SBATCH -N 4
+#SBATCH -n 4
 #SBATCH --partition dev
 #SBATCH --constraint m5a4xlarge
 #SBATCH --time 1-00:00:00
 #SBATCH --exclusive
 
-spack install \
+# First try to install with the binary cache
+srun -v -v -N 4 -n 4 spack install \
+  -v -y \
+  --deprecated \
   --no-check-signature \
   --no-checksum \
   --use-cache \
